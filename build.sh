b770813fbadd21f1544442c4cb55e756e39c1fcf
Jonne Lehtinen <jms@modeemi.fi>
2013-02-02 16:55:10 +0200

    Load plugins from native library dir

--- build.sh
+++ build.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
 ndk-build $@ APP_ABI=$ABI
-mkdir -p assets/plugins
-rm -f assets/plugins/*
-for i in android_output file flac id3v2 mad tremor curl icymetaint modplug faad mp4 equalizer opus
-do
-	mv libs/$ABI/lib$i.so assets/plugins/lib$i.so ;
-done
--- src/org/xmms2/server/Server.java
+++ src/org/xmms2/server/Server.java
@@ -1,19 +1,12 @@
 package org.xmms2.server;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.Service;
 import android.content.*;
-import android.content.res.AssetManager;
 import android.media.AudioManager;
 import android.os.*;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -27,7 +20,6 @@ public class Server extends Service implements NotificationUpdater
     public static final String ACTION_START_CLIENT = "org.xmms2.server.action.START_CLIENT";
     private HeadsetReceiver headsetReceiver;
     private Thread serverThread;
-    private String pluginPath;
     private boolean running;
     private static boolean storageAvailable;
     private BroadcastReceiver storageStateReceiver = new BroadcastReceiver() {
@@ -154,28 +146,6 @@ public class Server extends Service implements NotificationUpdater
         mediaObserver = new MediaObserver(Environment.getExternalStorageDirectory().getAbsolutePath());
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-        File pluginsDirOut = new File(getFilesDir(), "/plugins/");
-        pluginPath = pluginsDirOut.getAbsolutePath();
-        if (!pluginsDirOut.exists() && !pluginsDirOut.mkdirs()) {
-            throw new RuntimeException();
-        }
-
-        AssetManager am = getResources().getAssets();
-
-        try {
-            for (String plugin : am.list("plugins")) {
-                File out = new File(pluginsDirOut, plugin);
-                if (out.exists()) {
-                    continue;
-                }
-
-                InputStream stream = am.open("plugins/" + plugin, AssetManager.ACCESS_RANDOM);
-                long length = stream.skip(Long.MAX_VALUE);
-
-                copyFile(am.open("plugins/" + plugin), out, length);
-            }
-        } catch (IOException ignored) {}
-
         mediaButtonEventHandler = new ComponentName(this, MediaButtonEventHandler.class);
         audioManager.registerMediaButtonEventReceiver(mediaButtonEventHandler);
 
@@ -197,29 +167,6 @@ public class Server extends Service implements NotificationUpdater
         filter.addAction(AudioManager.ACTION_AUDIO_BECOMING_NOISY);
         filter.addAction(Intent.ACTION_HEADSET_PLUG);
         registerReceiver(headsetReceiver, filter);
-    }
-
-    private static void copyFile(InputStream input, File output, long length) throws IOException
-    {
-        if (!output.exists() && !output.createNewFile()) {
-            throw new IOException("Could not create file");
-        }
-
-        ReadableByteChannel source = null;
-        FileChannel destination = null;
-
-        try {
-            source = Channels.newChannel(input);
-            destination = new FileOutputStream(output).getChannel();
-            destination.transferFrom(source, 0, length);
-        } finally {
-            if (source != null) {
-                source.close();
-            }
-            if (destination != null) {
-                destination.close();
-            }
-        }
     }
 
     @Override
@@ -270,9 +217,14 @@ public class Server extends Service implements NotificationUpdater
         return getExternalFilesDir(null).getAbsolutePath();
     }
 
+    @SuppressLint("NewApi")
     private String getPluginPath()
     {
-        return pluginPath;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            return getApplicationInfo().nativeLibraryDir;
+        } else {
+            return getApplicationInfo().dataDir + "/lib";
+        }
     }
 
     private String getBrowseRoot()
